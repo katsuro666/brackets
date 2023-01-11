0c4aa9b623b2c3e9f11b21658d115cb369130256
katsuro666 <kreepinoutdakut@gmail.com>
2023-01-11 14:58:03 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let parsedBracketsConfig = bracketsConfig.map(item => {
+    return `${item[0]}${item[1]}`
+  })
+  
+  let prevLength = str.length
+
+  while (str.length) {
+    parsedBracketsConfig.forEach(item => {
+      str = str.replace(item, '')
+    })
+
+    if (str.length === prevLength) {
+      return false
+    }
+    
+    prevLength = str.length
+  }
+
+  return true
 }
